a7272f84ca6854eae1f3f2466250ccfeb999d206
Samuel Petcher <sampetcher@gmail.com>
2022-12-13 16:13:43 +0000

    Implement /api/articles endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,7 +10,7 @@ afterAll(() => pool.end());
 beforeEach(() => seed(testData));
 
 
-describe.only('GET /api/topics', () => {
+describe('GET /api/topics', () => {
 	test('Should return a list of topics in the form of an array', () => {
 		return request(app).get('/api/topics')
 			.expect(200)
@@ -22,6 +22,26 @@ describe.only('GET /api/topics', () => {
 						description: expect.any(String),
 					}))
 			})	
+			})
+	})
+})
+describe('GET /api/articles', () => {
+	test('Should return a list of articles', () => {
+		return request(app).get('/api/articles')
+			.expect(200)
+			.then( (response) => {
+				expect(response.body.articles.length).toBe(12)	
+				response.body.articles.forEach( (article) => {
+					expect(article).toEqual(expect.objectContaining({
+						author: expect.any(String),
+						title: expect.any(String),
+						article_id: expect.any(Number),
+						topic: expect.any(String),
+						created_at: expect.any(String),
+						votes: expect.any(Number), 
+						comment_count: expect.any(String),
+					}))
+			})
 			})
 	})
 })
--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const { getTopics } = require('./controllers/news.controllers');
+const { getTopics, getArticles } = require('./controllers/news.controllers');
 const app = express();
 
 app.get('/api/topics', getTopics)
+app.get('/api/articles', getArticles)
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send('Server Error!');
+});
 
 module.exports = app
